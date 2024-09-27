08a0e52e7af15e8f147356e26d17862119b76e9d
Stefan Mächler <stefan.maechler@westerm.com>
2024-09-27 22:38:36 +0200

    Improve support for fields in forms

--- triweb/utils/form.py
+++ triweb/utils/form.py
@@ -3,11 +3,12 @@ import re
 
 class Form(object):
 
-    def __init__(self, name, fields):
+    def __init__(self, name, field_names=None):
         self.name = name
         self.fields = []
-        for field in fields:
-            self.fields.append(self.Field(field, form=self))
+        if field_names is not None:
+            for name in field_names:
+                self.add_field(self.Field(name))
         self.validated = False
         self.valid = False
 
@@ -16,6 +17,12 @@ class Form(object):
             if field.name == name:
                 return field
         raise AttributeError()
+
+    def add_field(self, field):
+        field.form = self
+        if field.validator is None:
+            field.validator = getattr(self, f'do_validate_{field.name}', None)
+        self.fields.append(field)
 
     def copy_from(self, model):
         for field in self.fields:
@@ -26,18 +33,30 @@ class Form(object):
     def copy_to(self, model):
         pass
 
-    def validate_each(self, params):
+    def validate_each(self, params, **kw):
         self.valid = True
         for field in self.fields:
-            field.value = params.get(field.name, None)
-            yield field
+            field.get_from_params(params)
+            try:
+                field.validate(**kw)
+            except Form.Field.Ignore:
+                field.ignore = True
+                continue
+            except Form.Field.NoValidator:
+                yield field
+            field.validated = True
             if not field.is_valid():
                 self.valid = False
         self.validated = True
 
+    def validate(self, params, **kw):
+        for field in self.validate_each(params, **kw):
+            field.err_msg = 'Dieses Eingabefeld ist ungültig!'
+        return self.is_valid()
+
     def reset(self):
         for field in self.fields:
-            field.value = ''
+            field.reset()
         self.validated = False
         self.valid = False
 
@@ -50,24 +69,44 @@ class Form(object):
 
     class Field(object):
 
+        TYPE = 'custom'
+        DEFAULT = ''
         EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
-        def __init__(self, name, form=None):
+        def __init__(self, name, validator=None):
             self.name = name
-            self.value = ''
-            self.err_msg = ''
-            self.form = form
+            self.validator = validator
+            self.form = None
+            self.reset()
+
+        def reset(self):
+            self.value = self.DEFAULT
             self.ignore = False
+            self.err_msg = ''
+            self.validated = False
 
         def is_email(self):
             return re.fullmatch(self.EMAIL_REGEX, self.value)
 
         def is_validated(self):
-            return not self.ignore \
-                    and (self.form is not None and self.form.validated)
+            return not self.ignore and self.validated
 
         def is_valid(self):
             return self.err_msg is None or len(self.err_msg) == 0
+
+        def get_from_params(self, params):
+            self.value = params.get(self.name, self.DEFAULT)
+
+        def validate(self, **kw):
+            if self.validator == False:
+                raise Form.Field.Ignore()
+            if self.validator is None:
+                raise Form.Field.NoValidator()
+            try:
+                self.validator(self, **kw)
+            except Form.Field.Error as err:
+                self.err_msg = err.msg
+            self.validated = True
 
         def get_valid_cls(self):
             if not self.is_validated():
@@ -79,3 +118,49 @@ class Form(object):
 
         def __repr__(self):
             return f'({self.name}, {self.value}, {self.err_msg})'
+
+
+        class NoValidator(Exception):
+            pass
+
+        class Ignore(Exception):
+            pass
+
+        class Error(Exception):
+
+            def __init__(self, msg):
+                self.msg = msg
+
+
+    class TextField(Field):
+
+        TYPE = 'text'
+
+        def __init__(self, name, allow_empty=False):
+            super().__init__(name)
+            self.allow_empty = allow_empty
+
+        def validate(self, **kw):
+            if self.validator == False:
+                raise Form.Field.Ignore()
+            if self.value is None or len(self.value) == 0:
+                if self.allow_empty:
+                    raise Form.Field.Ignore()
+                self.err_msg = 'Dieses Feld darf nicht leer sein!'
+            elif self.validator is not None:
+                try:
+                    self.validator(self, **kw)
+                except Form.Field.Error as err:
+                    self.err_msg = err.msg
+            self.validated = True
+
+    class SelectMultiple(Field):
+
+        TYPE = 'select_multiple'
+        DEFAULT = []
+
+        def __init__(self, name, validator=None):
+            super().__init__(name, validator)
+
+        def get_from_params(self, params):
+            self.value = params.getall(self.name)
