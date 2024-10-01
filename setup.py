af0df21a6e9112cb74cdc0d1ee456ca696c746df
Stefan Mächler <stefan.maechler@westerm.com>
2024-10-01 21:55:27 +0200

    Increase version to V0.11

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='triweb',
-    version='0.10',
+    version='0.11',
     description='Webseite für das Ressort Triebfahrzeuge des DVZO',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
